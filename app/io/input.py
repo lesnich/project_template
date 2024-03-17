98218bc8a35d3cad4d93f7bfe3163bbde4d29f57
Sofiia Kolesnichenko <kolesnichenko.sophia@gmail.com>
2024-03-17 16:02:05 +0100

    IO added

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def input_from_console():
+    pass
+
+
+def read_from_file_builtin(filename):
+    pass
+
+
+def read_from_file_pandas(filename):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+
+
+def output_to_console(text):
+    pass
+
+
+def write_to_file_builtin(filename, content):
+    pass
